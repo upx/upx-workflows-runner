1eb2fcc0af713acfb3c9be697bc4852b1e2a2b0e
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2024-03-14 17:02:46 +0100

    Update.

--- .github/workflows/build_lzo.sh
+++ .github/workflows/build_lzo.sh
@@ -13,24 +13,33 @@ if ! test -d lzo-2.10; then true;
 fi
 cd lzo-2.10
 
-mkdir -p build/release
-cd build/release
+build_lzo() {
+(
+    mkdir -p build/$1
+    cd build/$1
 
-# note that "gcc" can be a symlink to "clang"
-CC=gcc CXX=g++ cmake ../.. -DCMAKE_VERBOSE_MAKEFILE=ON
-if test -f Makefile; then true;
-    # prefer "make" as old CMake versions do not have "--parallel"
-    make -j
-else
-    cmake --build . --parallel --config Release
-fi
+    # note that "gcc" can be a symlink to "clang"
+    CC=gcc CXX=g++ cmake ../.. -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=$2 -DENABLE_STATIC=$3 -DENABLE_SHARED=$4
+    if test -f Makefile; then true;
+        # prefer "make" as old CMake versions do not have "--parallel"
+        make -j
+    else
+        cmake --build . --parallel --config $2
+    fi
 
-ctest -C Release
+    ctest -C $2
 
-if test -f Makefile; then true;
-    # old CMake versions get confused by "--config Release" here
-    DESTDIR="$PWD/Install with cmake" cmake --install .
-    DESTDIR="$PWD/Install with make"  make install
-else
-    DESTDIR="$PWD/Install with cmake" cmake --install . --config Release
-fi
+    if test -f Makefile; then true;
+        # old CMake versions get confused by "--config Release" here
+        DESTDIR="$PWD/Install with cmake" cmake --install .
+        DESTDIR="$PWD/Install with make"  make install
+    else
+        DESTDIR="$PWD/Install with cmake" cmake --install . --config $2
+    fi
+)
+}
+
+build_lzo release-00 Release OFF OFF
+build_lzo release-10 Release ON OFF
+build_lzo release-01 Release OFF ON
+build_lzo release-11 Release ON ON
